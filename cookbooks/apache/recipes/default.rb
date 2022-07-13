3991014a19a639803fc71800be98e37f23032b34
root <root@ip-172-31-11-195.ap-south-1.compute.internal>
2022-07-13 04:02:15 +0000

    apache cb

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,11 +6,22 @@
 #
 
 package 'httpd'
+package 'tomcat'
+package 'tomcat-webapps'
 
-service 'httpd' do
+service 'tomcat' do
   action [:enable, :start]
+end
+
+template '/etc/httpd/conf.d/proxy.conf' do
+  source 'proxy.conf.erb'
 end
 
 template '/var/www/html/index.html' do
   source 'index.html.erb'
 end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
