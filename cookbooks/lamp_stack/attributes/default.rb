eaa11bac3c6ee9ba081bf838179aeff96415f381
root <root@ip-172-31-11-195.ap-south-1.compute.internal>
2022-07-13 05:43:49 +0000

    lamp-cb

---
+++ cookbooks/lamp_stack/attributes/default.rb
@@ -0,0 +1,2 @@
+default["lamp_stack"]["sites"]["accenture.com"] = { "port" => 80, "servername" => "accenture.com", "serveradmin" => "webmaster@accenture.com" }
+default["lamp_stack"]["sites"]["accenture.org"] = { "port" => 80, "servername" => "accenture.org", "serveradmin" => "webmaster@accenture.org" }
---
+++ cookbooks/lamp_stack/recipes/apache.rb
@@ -0,0 +1,39 @@
+package "httpd" do
+ action :install
+end
+
+service "httpd" do
+ action [:enable, :start]
+end
+
+node["lamp_stack"]["sites"].each do |sitename, data|
+
+  document_root = "/var/www/html/#{sitename}"
+
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+  
+  directory "/var/www/html/#{sitename}/public_html" do
+    action :create
+  end
+
+  directory "/var/www/html/#{sitename}/logs" do
+    action :create
+  end
+
+
+  template "/etc/httpd/conf.d/#{sitename}.conf" do
+    source "virtualhosts.erb"
+    mode "0644"
+    variables(
+     :document_root => document_root,
+     :port => data["port"],
+     :serveradmin => data["serveradmin"],
+     :servername => data["servername"]
+    )
+ end
+# notifies :restart, "service[httpd]"
+end
+
